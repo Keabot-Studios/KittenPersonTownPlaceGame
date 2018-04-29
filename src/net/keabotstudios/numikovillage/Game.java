3d9b80ed468ddce14072ac54f5fe9d8f41afcc7a
Kortranis Wellspringsworth XXVII <legomaster8998@gmail.com>
2018-04-29 00:56:12 -0400

    Made with pink magic salt!

--- src/net/keabotstudios/numikovillage/Game.java
+++ src/net/keabotstudios/numikovillage/Game.java
@@ -1,5 +1,34 @@
 package net.keabotstudios.numikovillage;
 
+import org.lwjgl.glfw.GLFW;
+
 public class Game {
 
+	private DisplayManager display;
+	
+	public Game() {
+		this.init();
+		this.loop();
+	}
+
+	private void init() {
+		display = new DisplayManager();
+		display.initalize(1280, 720, "Fuck.", this::kCallback);
+	}
+	
+	private void kCallback(long window, int key, int scanCode, int action, int mods) {
+		if (key == GLFW.GLFW_KEY_ESCAPE && action == GLFW.GLFW_PRESS)
+			display.close();
+	}
+	
+	public static void main(String[] args) {
+		new Game();
+	}
+	
+	private void loop() {
+		while (!display.shouldClose()) {
+			display.update();
+		}
+		display.destroy();
+	}
 }
