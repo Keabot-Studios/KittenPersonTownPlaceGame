d88be5cf3a5e483264f8d2db2f3b8426b51e3f2f
Richard Abbott <richie0415@hotmail.com>
2018-04-28 22:34:16 -0400

    Added Display Manager.

---
+++ src/net/keabotstudios/numikovillage/DisplayManager.java
@@ -0,0 +1,134 @@
+package net.keabotstudios.numikovillage;
+
+import static org.lwjgl.glfw.Callbacks.glfwFreeCallbacks;
+import static org.lwjgl.glfw.GLFW.GLFW_FALSE;
+import static org.lwjgl.glfw.GLFW.GLFW_RESIZABLE;
+import static org.lwjgl.glfw.GLFW.GLFW_TRUE;
+import static org.lwjgl.glfw.GLFW.GLFW_VISIBLE;
+import static org.lwjgl.glfw.GLFW.glfwCreateWindow;
+import static org.lwjgl.glfw.GLFW.glfwDefaultWindowHints;
+import static org.lwjgl.glfw.GLFW.glfwDestroyWindow;
+import static org.lwjgl.glfw.GLFW.glfwGetPrimaryMonitor;
+import static org.lwjgl.glfw.GLFW.glfwGetVideoMode;
+import static org.lwjgl.glfw.GLFW.glfwGetWindowSize;
+import static org.lwjgl.glfw.GLFW.glfwInit;
+import static org.lwjgl.glfw.GLFW.glfwMakeContextCurrent;
+import static org.lwjgl.glfw.GLFW.glfwPollEvents;
+import static org.lwjgl.glfw.GLFW.glfwSetErrorCallback;
+import static org.lwjgl.glfw.GLFW.glfwSetKeyCallback;
+import static org.lwjgl.glfw.GLFW.glfwSetWindowPos;
+import static org.lwjgl.glfw.GLFW.glfwShowWindow;
+import static org.lwjgl.glfw.GLFW.glfwSwapBuffers;
+import static org.lwjgl.glfw.GLFW.glfwSwapInterval;
+import static org.lwjgl.glfw.GLFW.glfwTerminate;
+import static org.lwjgl.glfw.GLFW.glfwWindowHint;
+import static org.lwjgl.glfw.GLFW.glfwWindowShouldClose;
+import static org.lwjgl.system.MemoryStack.stackPush;
+import static org.lwjgl.system.MemoryUtil.NULL;
+
+import java.nio.IntBuffer;
+
+import org.lwjgl.Version;
+import org.lwjgl.glfw.GLFWErrorCallback;
+import org.lwjgl.glfw.GLFWKeyCallbackI;
+import org.lwjgl.glfw.GLFWVidMode;
+import org.lwjgl.opengl.GL;
+import org.lwjgl.system.MemoryStack;
+
+public class DisplayManager {
+	
+	private long window;
+	private int width, height;
+
+	public void initalize(int width, int height, String title, GLFWKeyCallbackI cbfun) {
+		System.out.println("Intializing LWJGL and GLFW... (" + Version.getVersion() + ")");
+		GLFWErrorCallback.createPrint(System.err).set();
+
+		if ( !glfwInit() )
+			throw new IllegalStateException("Unable to initialize GLFW.");
+		System.out.println("Initalized GLFW.");
+
+		glfwDefaultWindowHints();
+		glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
+		glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
+		
+		System.out.println("Creating GLFW window...");
+		window = glfwCreateWindow(width, height, title, NULL, NULL);
+		if ( window == NULL )
+			throw new RuntimeException("Failed to create the GLFW window.");
+
+		glfwSetKeyCallback(window, cbfun);
+
+		try ( MemoryStack stack = stackPush() ) {
+			IntBuffer pWidth = stack.mallocInt(1);
+			IntBuffer pHeight = stack.mallocInt(1);
+
+			glfwGetWindowSize(window, pWidth, pHeight);
+			GLFWVidMode vidmode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+			
+			width = pWidth.get(0);
+			height = pHeight.get(0);
+
+			glfwSetWindowPos(
+				window,
+				(vidmode.width() - pWidth.get(0)) / 2,
+				(vidmode.height() - pHeight.get(0)) / 2
+			);
+		}
+		
+
+		glfwMakeContextCurrent(window);
+		glfwSwapInterval(1);
+
+		glfwShowWindow(window);
+		GL.createCapabilities();
+		
+		System.out.println("Created GLFW window.");
+		
+		System.out.println("Initialization complete! Go have fun with some kitties!");
+	}
+	
+	public boolean shouldClose() {
+		if(window == NULL)
+			return true;
+		return glfwWindowShouldClose(window);
+	}
+	
+	public void update() {
+		if(window == NULL)
+			return;
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+		
+		try ( MemoryStack stack = stackPush() ) {
+			IntBuffer pWidth = stack.mallocInt(1);
+			IntBuffer pHeight = stack.mallocInt(1);
+			glfwGetWindowSize(window, pWidth, pHeight);
+			width = pWidth.get(0);
+			height = pHeight.get(0);
+		}
+	}
+	
+	public void close() {
+		if(window == NULL)
+			return;
+		glfwDestroyWindow(window);
+		glfwFreeCallbacks(window);
+		
+		glfwTerminate();
+		glfwSetErrorCallback(null).free();
+	}
+	
+	public int getWidth() {
+		return width;
+	}
+	
+	public int getHeight() {
+		return height;
+	}
+	
+	public float getAspect() {
+		return (float) width / (float) height;
+	}
+
+}
